355e30569095d4f233998aa2606574b7b80490f0
semimikoh <ejffjeosms@gmail.com>
2026-03-31 09:59:14 +0900

    fs: validate mode in CreateReadStream and CreateWriteStream
     Large mode values (e.g. 2176057344) that exceed the Int32 range
      but fit within UInt32 cause a C++ assertion failure (args[2]->IsInt32())
      instead of throwing a RangeError.
    
      This is a regression introduced by #52050 which moved mode validation
      to C++. Fix by:
      1. Changing parseFileMode() to use validateInt32 instead of
         validateUint32 to match the C++ Int32 expectation.
      2. Adding parseFileMode() calls in ReadStream/WriteStream constructors.
    
      Fixes: https://github.com/nodejs/node/issues/62516

--- test/parallel/test-file-validate-mode-flag.js
+++ test/parallel/test-file-validate-mode-flag.js
@@ -1,10 +1,13 @@
 'use strict';
 
 // Checks for crash regression: https://github.com/nodejs/node/issues/37430
+// and https://github.com/nodejs/node/issues/62516
 
 const common = require('../common');
 const assert = require('assert');
 const {
+  createReadStream,
+  createWriteStream,
   open,
   openSync,
   promises: {
@@ -15,11 +18,19 @@ const {
 // These should throw, not crash.
 const invalid = 4_294_967_296;
 
+// Value within UInt32 range but exceeding Int32 range.
+// Regression test for https://github.com/nodejs/node/issues/62516
+const invalidInt32 = 2_176_057_344;
+
 assert.throws(() => open(__filename, invalid, common.mustNotCall()), {
   code: 'ERR_OUT_OF_RANGE'
 });
 
 assert.throws(() => open(__filename, 0, invalid, common.mustNotCall()), {
+  code: 'ERR_OUT_OF_RANGE'
+});
+
+assert.throws(() => open(__filename, 0, invalidInt32, common.mustNotCall()), {
   code: 'ERR_OUT_OF_RANGE'
 });
 
@@ -31,6 +42,10 @@ assert.throws(() => openSync(__filename, 0, invalid), {
   code: 'ERR_OUT_OF_RANGE'
 });
 
+assert.throws(() => openSync(__filename, 0, invalidInt32), {
+  code: 'ERR_OUT_OF_RANGE'
+});
+
 assert.rejects(openPromise(__filename, invalid), {
   code: 'ERR_OUT_OF_RANGE'
 }).then(common.mustCall());
@@ -38,3 +53,25 @@ assert.rejects(openPromise(__filename, invalid), {
 assert.rejects(openPromise(__filename, 0, invalid), {
   code: 'ERR_OUT_OF_RANGE'
 }).then(common.mustCall());
+
+assert.rejects(openPromise(__filename, 0, invalidInt32), {
+  code: 'ERR_OUT_OF_RANGE'
+}).then(common.mustCall());
+
+// createReadStream and createWriteStream should also validate mode.
+// Regression test for https://github.com/nodejs/node/issues/62516
+assert.throws(() => createReadStream(__filename, { mode: invalidInt32 }), {
+  code: 'ERR_OUT_OF_RANGE'
+});
+
+assert.throws(() => createWriteStream(__filename, { mode: invalidInt32 }), {
+  code: 'ERR_OUT_OF_RANGE'
+});
+
+assert.throws(() => createReadStream(__filename, { mode: invalid }), {
+  code: 'ERR_OUT_OF_RANGE'
+});
+
+assert.throws(() => createWriteStream(__filename, { mode: invalid }), {
+  code: 'ERR_OUT_OF_RANGE'
+});
